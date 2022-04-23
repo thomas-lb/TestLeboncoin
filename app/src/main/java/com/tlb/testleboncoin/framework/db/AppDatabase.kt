20e1d89e6ab19c0ac322a410e8e176fc6e6e47c5
Thomas Le Bihan <thomas.lebihan@shopopop.com>
2022-04-23 22:34:42 +0200

    Fix warning

--- app/src/main/java/com/tlb/testleboncoin/framework/db/AppDatabase.kt
+++ app/src/main/java/com/tlb/testleboncoin/framework/db/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.tlb.testleboncoin.framework.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [TitleEntity::class], version = 1)
+@Database(entities = [TitleEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun titleDao(): TitleDao
 }
